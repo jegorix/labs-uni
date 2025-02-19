e427a57cf32e716f151daaa0c0623843b4ac3f0c
jegorix <0311egor@gmail.com>
2025-02-19 21:51:28 +0300

    first-task-complete

--- 9lab/file_text_task-1.c
+++ 9lab/file_text_task-1.c
@@ -1,5 +1,5 @@
 // С клавиатуры заполнить файл словами. Вывести содержимое файла на экран.
-// Распечатать второе и предпоследнее слова.
+
 // Поменять местами самое длинное слово и самое коротко.
 #include <stdio.h>
 #include <stdlib.h>
@@ -48,6 +48,12 @@ void add_data(char* file_name, char* user_input)
 char* get_words(void)
 {
     char* input = malloc(256 * sizeof(char));
+    // if (input == NULL)
+    // {
+    //     printf("Ошибка выделения памяти!\n");
+    //     exit(1);
+    // }
+
     printf("Введите слова:\n");
     fgets(input, 256, stdin);
     return input;
@@ -63,11 +69,12 @@ int main(int argc, char* argv[])
         return 1;
     }
 
-    char *file_name = argv[1];
+    char* file_name = argv[1];
     char* user_input = get_words();
     add_data(file_name, user_input);
     data_output(file_name);
     free(user_input);
+
 
     
 return 0;
