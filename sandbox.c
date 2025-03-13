73067bce4bafe6ca43e57b0bacaaeca21426e37a
jegorix <0311egor@gmail.com>
2025-03-13 23:05:42 +0300

    fix find remainder execution

--- sandbox.c
+++ sandbox.c
@@ -81,3 +81,44 @@ int main(void)
     return 0;
 
 }
+
+
+
+
+
+// int main(void)
+// {
+//     int num;
+//     printf("Введите число: \n");
+//     scanf("%d", &num);
+
+//     int beg_num = num;
+
+//     if(num < 0)
+//     {
+//         num = -num;
+//     }
+
+
+//     while (num >= 0)
+//     {
+//         if (num == 1)
+//         {
+//             printf("Остаток от деления числа %d на 2 = 1 \n", beg_num);
+//             break;
+//         }
+
+//         if (num == 0)
+//         {
+//             printf("Остаток от деления числа %d на 2 = 0 \n", beg_num);
+//             break;
+//         }
+
+//         num -= 2;
+//     }
+    
+//     printf("Проверка: %d\n", beg_num % 2);
+
+
+//     return 0;
+// }
