65b1e211460a640847aedeace8940e2b8a4ed945
jegorix <0311egor@gmail.com>
2025-03-14 11:44:50 +0300

    add figure_name

--- 10lab/struct_task_2.c
+++ 10lab/struct_task_2.c
@@ -64,10 +64,30 @@ typedef struct
 typedef struct
 {
     int square;
-    char* name[20];
+    char* name;
     Perimiter perm;
     char* color;
 } Square;
+
+
+
+void handle_figure_input(int figure_count)
+{
+    char* fig_name = malloc(256 * sizeof(char));
+    Square* figure = malloc(sizeof(Square) * figure_count); // create figures array
+
+    for(int i = 0; i < figure_count; i++)
+    {
+        printf("Введите имя вашей фигуры № %d:\n", i + 1);
+        fgets(fig_name, 256, stdin);
+        figure[i].name = malloc(strlen(fig_name) + 1);
+        strcpy(figure[i].name, fig_name);
+        printf("%s\n",figure[i].name);
+
+    }
+
+    
+}
 
 
 
@@ -75,7 +95,7 @@ void identify_figure_count()
 {
     char* user_choice = malloc(50 * sizeof(char));
     printf("Введите количество фигур:\n");
-    int figure_count = execute_verification();
+    int figure_count = execute_verirfication(min_limit, max_limit);
     printf("Ввести фигуры вручную - 1. Рандомные фигуры - любая клавиша");
     fgets(user_choice, sizeof(user_choice), stdin);
 
@@ -85,18 +105,13 @@ void identify_figure_count()
         handle_figure_input(figure_count);
         break;
 
-        default:
-        random_figure_input(figure_count);
-        break;
+        // default:
+        // random_figure_input(figure_count);
+        // break;
 
     }
 
 }
-
-
-handle_figure_input(figure_count)
-
-
 
 
 
