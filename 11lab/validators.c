ab987d59c42c44d4e90f546e4be4aa18f5dee1ed
jegorix <0311egor@gmail.com>
2025-03-28 20:26:16 +0300

    fix validators

--- 11lab/validators.c
+++ 11lab/validators.c
@@ -4,7 +4,69 @@
 
 #include "validators.h"
 
-int positivenum(int num)
+
+int execute_verirfication(int min_limit, int max_limit)
 {
-  return num + 100;
+  char input[100];
+  int number;
+  char symbol;
+
+  while(1)
+  {
+    if(fgets(input, sizeof(input), stdin) == NULL)
+    {
+      printf("Ошибка ввода! Попробуйте еще раз!\n");
+      continue;
+    }
+
+    if (sscanf(input, "%d %c", &number, &symbol) != 1)
+    {
+      printf("Ошибка ввода! Введите целое число:\n");
+      continue;
+    }
+
+    if (number > max_limit || number < min_limit)
+    {
+      printf("Ошибка ввода! Введите число из диапазона [%d, %d]:\n", min_limit, max_limit);
+      continue;
+    }
+
+    return number;
+
+  }
+
+}
+
+
+
+float execute_verirfication_float(float min_limit, float max_limit)
+{
+  char input[100];
+  float number;
+  char symbol;
+
+  while(1)
+  {
+    if(fgets(input, sizeof(input), stdin) == NULL)
+    {
+      printf("Ошибка ввода! Попробуйте еще раз!\n");
+      continue;
+    }
+
+    if (sscanf(input, "%f %c", &number, &symbol) != 1)
+    {
+      printf("Ошибка ввода! Введите число с плавающей запятой:\n");
+      continue;
+    }
+
+    if (number > max_limit || number < min_limit)
+    {
+      printf("Ошибка ввода! Введите число из диапазона [%.2f, %.2f]:\n", min_limit, max_limit);
+      continue;
+    }
+
+    return number;
+
+  }
+
 }
--- 11lab/validators.h
+++ 11lab/validators.h
@@ -4,7 +4,18 @@
 
 #ifndef VALIDATORS_H
 #define VALIDATORS_H
+#include <stdio.h>
 
-int positivenum(int num);
+enum
+{
+    max_limit = 2147483647,
+    min_limit = -2147483647,
+    rand_max_limit = 1000000,
+    rand_min_limit = -1000000,
+  };
+
+int execute_verirfication(int min_limit, int max_limit);
+float execute_verirfication_float(float min_limit, float max_limit);
+
 
 #endif //VALIDATORS_H
