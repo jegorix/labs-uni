bad2b62e51d51368a9687266215709f92254b6ce
jegorix <0311egor@gmail.com>
2025-03-28 22:04:22 +0300

    tasks choice

--- 11lab/user_choice.c
+++ 11lab/user_choice.c
@@ -3,9 +3,25 @@
 //
 
 #include "user_choice.h"
+#include "integer_stack.h"
 #include <stdio.h>
 
 void user_choice_action()
     {
-      printf("Please enter an integer: ");
+      char user_input[50];
+      printf("Выберите номер задачи:\n");
+      printf("Задача № 1 - введите цифру 1\n");
+      printf("Задача № 2 - введите цифру 2\n");
+      printf("Задача № 3 - любая другая клавиша \n");
+
+      fgets(user_input, 50, stdin);
+
+//      switch(user_input[0])
+//      {
+//        case '1':
+//
+//      };
+
+
+
     }
