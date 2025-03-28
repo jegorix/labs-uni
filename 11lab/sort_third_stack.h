32c682831c866e7b9091cd0cb013c5244c8b92f6
jegorix <0311egor@gmail.com>
2025-03-29 00:06:12 +0300

    create stack fill by increasing

---
+++ 11lab/sort_third_stack.h
@@ -0,0 +1,15 @@
+//
+// Created by Macbook on 28.03.25.
+//
+
+#ifndef SORT_THIRD_STACK_H
+#define SORT_THIRD_STACK_H
+
+#include "integer_stack.h"
+
+void create_multy_stacks();
+OBJ* declare_multy_stack(int stack_size, OBJ* top, int stack_number);
+void build_third_stack();
+int check_increasing(int data, int stack_num)
+
+#endif //SORT_THIRD_STACK_H
