8e6f77777e0f49feb390e25b493b6a162eeaae14
jegorix <0311egor@gmail.com>
2025-03-28 22:03:17 +0300

    add struct create/declare/push stacks

--- 11lab/integer_stack.h
+++ 11lab/integer_stack.h
@@ -5,7 +5,14 @@
 #ifndef INTEGER_STACK_H
 #define INTEGER_STACK_H
 
-void integer_stack_init();
+typedef struct stack_object
+{
+    int data;
+    struct stack_object* next;
+} OBJ;
 
+void create_stacks();
+OBJ* declare_stack(int stack_size, OBJ* top, int stack_number);
+OBJ* stack_push(OBJ* top, int data);
 
 #endif //INTEGER_STACK_H
