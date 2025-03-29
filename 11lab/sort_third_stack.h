e9c983c19a3a58e4597be913d4094e9d94fd9f54
jegorix <0311egor@gmail.com>
2025-03-29 14:32:25 +0300

    add check_increasing, user_action and make_third_stack

--- 11lab/sort_third_stack.h
+++ 11lab/sort_third_stack.h
@@ -10,6 +10,8 @@
 void create_multy_stacks();
 OBJ* declare_multy_stack(int stack_size, OBJ* top, int stack_number);
 void build_third_stack();
-int check_increasing(int data, int stack_num)
+int check_increasing(int data, int stack_num);
+void user_action(OBJ* top_1, OBJ* top_2, OBJ* top_3);
+OBJ* make_third_stack(int stack_size_1, int stack_size_2, OBJ* top_1, OBJ* top_2);
 
 #endif //SORT_THIRD_STACK_H
