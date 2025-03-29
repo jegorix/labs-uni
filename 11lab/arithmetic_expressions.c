c75c2ec0c107844a9b34fc601590c57ab7272080
jegorix <0311egor@gmail.com>
2025-03-29 22:46:42 +0300

    add possibility to choose existing file

--- 11lab/arithmetic_expressions.c
+++ 11lab/arithmetic_expressions.c
@@ -50,22 +50,73 @@ void add_data(char* file_name, char* user_input)
 
 
 
-void create_expressions(char* file_name)
+
+//void calculate_expressins(char* string)
+//    {
+//
+//    }
+
+
+
+
+
+
+void create_expressions(char* file_name, int* expressions_count)
         {
   printf("Сколько арифметических выражений вы хотите записать?");
-  int expressions_count = execute_verification(0, max_limit);
+  *expressions_count = execute_verification(0, max_limit);
   printf("Введите арифметическое выражение, используя ' -, +, *, / и скобки () ' ");
-  for(int i = 0; i < expressions_count; i++)
+  for(int i = 0; i < *expressions_count; i++)
     {
     printf("%d-ое выражение:\n", i+1);
     char* user_expression = get_words();
+    user_expression[strcspn(user_expression, "\n")] = 0;
     add_data(file_name, user_expression);
     }
         }
 
+void extract_expressions(char* file_name, int expressions_count)
+            {
+  FILE* file = fopen(file_name, "r");
+  if(file == NULL)
+    {
+      printf("Не удалось открыть файл!\n");
+      return;
+    }
 
 
+  char* buffer = malloc(256 * sizeof(char));
+  for(int i = 0; i < expressions_count; i++)
+    {
+       fgets(buffer, 256, file);
+       char* result = calculate_expressions();
+       printf("%s\n", buffer);
 
+    }
+    free(buffer);
+
+fclose(file);
+            }
+
+
+ int count_sentence(char* file_name)
+                {
+   FILE* file = fopen(file_name, "r");
+   int sentence_count = 0;
+   char ch;
+   while((ch = fgetc(file)) != EOF)
+     {
+        if(ch == '\0' || ch == '\n' || ch == '=')
+          {
+
+          sentence_count++;
+          }
+     }
+
+    printf("%d\n", sentence_count);
+     fclose(file);
+     return sentence_count;
+                }
 
 
 void make_or_choose_file()
@@ -76,12 +127,30 @@ void make_or_choose_file()
   printf("Выбрать существующий файл и работать в нем - 2:\n");
   fgets(user_input, 50, stdin);
   char* file_name;
+  int expressions_count;
 
   switch (user_input[0])
   {
     case '1':
        file_name = make_file();
-       create_expressions(file_name);
+       create_expressions(file_name, &expressions_count);
+       extract_expressions(file_name, expressions_count);
+       free(file_name);
+       break;
+
+     case '2':
+       file_name = malloc(50);
+       printf("Введите имя существующего файла: ");
+       fgets(file_name, 50, stdin);
+       file_name[strcspn(file_name, "\n")] = 0;
+      expressions_count = count_sentence(file_name);
+      extract_expressions(file_name, expressions_count);
+
+      free(file_name);
+      break;
+
+
+
   }
 
 
