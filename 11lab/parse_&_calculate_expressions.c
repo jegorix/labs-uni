6deea366648082381b5ab5d9888e84a99ff97db9
jegorix <0311egor@gmail.com>
2025-03-30 13:59:04 +0300

    fix push(by double) add convert func

--- 11lab/parse_&_calculate_expressions.c
+++ 11lab/parse_&_calculate_expressions.c
@@ -11,6 +11,37 @@
 #include "integer_stack.h"
 #include "ctype.h"
 
+
+OBJ* stack_push_double(OBJ* top, double data)
+{
+  OBJ* temp = malloc(sizeof(OBJ));
+  temp->data = data;
+  temp->next = top;
+  return temp;
+}
+
+
+
+
+
+void convert_to_rpn(char* expression)
+    {
+  char* token = strtok(expression, " ");
+  while(token != NULL)
+      {
+        char* endptr;
+        double number = strtod(token, &endptr);
+        if (*endptr == '\0')
+          {
+          printf("%s\n", token);
+          }
+
+        token = strtok(NULL, " ");
+
+      }
+    }
+
+
 int is_operator(char c)
     {
   return (c == '+' || c == '-' || c == '*' || c == '/' );
@@ -24,11 +55,13 @@ void parse_expressions(char* expression)
   while(token != NULL)
     {
 
-    if(isdigit(token[0]) || (token[0] == '-' && isdigit(token[1])))
-      {
-      int number = atoi(token);
-      top = stack_push(top, number);
+    char* endptr;
+    double number = strtod(token, &endptr);
 
+    if (*endptr == '\0')
+      {
+      printf("Добавляю в стек: %lf\n", number);
+      top = stack_push_double(top, number);
       }
 
      else if (is_operator(token[0]))
@@ -39,11 +72,11 @@ void parse_expressions(char* expression)
             return;
             }
 
-            int b = top->data;
+            double b = top->data;
             top = stack_pop(top);
-            int a = top->data;
+            double a = top->data;
             top = stack_pop(top);
-            int result;
+            double result;
 
             switch (token[0])
             {
@@ -60,8 +93,8 @@ void parse_expressions(char* expression)
                   break;
             }
 
-        top = stack_push(top, result);
-        printf("%d %c %d = %d\n", a, token[0] , b, result);
+        top = stack_push_double(top, result);
+        printf("%.2lf %c %.2lf = %.2lf\n", a, token[0] , b, result);
 
         }
 
