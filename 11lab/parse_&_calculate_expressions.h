0c509bcf41af6ce3a98d09d2e9a8a294687d237f
jegorix <0311egor@gmail.com>
2025-03-30 21:01:32 +0300

    create new struct for double

--- 11lab/parse_&_calculate_expressions.h
+++ 11lab/parse_&_calculate_expressions.h
@@ -5,13 +5,19 @@
 #ifndef PARSE___CALCULATE_EXPRESSIONS_H
 #define PARSE___CALCULATE_EXPRESSIONS_H
 
-typedef struct
+typedef struct StackNode
     {
   char* data;
   struct StackNode* next;
-    } StractNode;
+    } StackNode;
+
+typedef struct ObjDouble
+        {
+  double value;
+  struct ObjDouble* next;
+        } ObjDouble;
 
 void parse_expressions(char* expression);
 int is_operator(char c);
-void convert_to_rpn(char* expression);
+void convert_to_rpn(char* expression, char* rpn_result);
 #endif //PARSE___CALCULATE_EXPRESSIONS_H
