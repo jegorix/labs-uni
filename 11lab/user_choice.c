35eced0c9c7af54954ad99f0f4f7bfa38f328794
jegorix <0311egor@gmail.com>
2025-04-04 12:48:39 +0300

    add infinity cycle

--- 11lab/user_choice.c
+++ 11lab/user_choice.c
@@ -10,14 +10,20 @@
 #include "arithmetic_expressions.h"
 void user_choice_action()
     {
+  int exit = 0;
+  while(!exit)
+  {
       char user_input[50];
+
       printf("Выберите номер задачи:\n");
       printf("Задача № 1 - введите цифру 1\n");
       printf("Задача № 2 - введите цифру 2\n");
       printf("Задача № 3 - введите цифру 3\n");
+      printf("Продолжить - введите цифру 4\n");
       printf("Выход - любая другая клавиша \n");
 
       fgets(user_input, 50, stdin);
+
 
       switch(user_input[0])
       {
@@ -36,13 +42,17 @@ void user_choice_action()
           make_or_choose_file();
         }break;
 
+         case '4':
+           continue;
+
         default:
           {
+          exit = 1;
           return;
           }
 
       }
 
 
-
+}
     }
