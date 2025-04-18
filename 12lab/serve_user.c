f77a32c67098e27704885c8e5738ee5b10483316
jegorix <0311egor@gmail.com>
2025-04-18 21:20:09 +0300

    create func - that operates users commands

---
+++ 12lab/serve_user.c
@@ -0,0 +1,10 @@
+//
+// Created by jegorix on 18.04.25.
+//
+
+#include "serve_user.h"
+
+int handle_user_choice(int user_choice)
+{
+    printf("execute task");
+}
---
+++ 12lab/serve_user.h
@@ -0,0 +1,12 @@
+//
+// Created by jegorix on 18.04.25.
+//
+
+#ifndef INC_12LAB_SERVE_USER_H
+#define INC_12LAB_SERVE_USER_H
+
+#include <stdio.h>
+
+int handle_user_choice(int user_choice);
+
+#endif //INC_12LAB_SERVE_USER_H
