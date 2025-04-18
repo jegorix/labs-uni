2c18767d95598217addf73becbe05e03d6524497
jegorix <0311egor@gmail.com>
2025-04-18 22:02:52 +0300

    handle 1st task

--- 12lab/serve_user.c
+++ 12lab/serve_user.c
@@ -3,8 +3,14 @@
 //
 
 #include "serve_user.h"
+#include "validators.h"
+#include "queue_actions.h"
 
 int handle_user_choice(int user_choice)
 {
-    printf("execute task");
+    switch (user_choice) {
+        case 1:
+            printf("\nЗадача №1\n");
+            queue_actions_menu();
+    }
 }
