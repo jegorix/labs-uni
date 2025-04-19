35d7a404daaf4a2013ddd92eec0d4a5443b4f958
jegorix <0311egor@gmail.com>
2025-04-19 15:04:59 +0300

    create second task choice

--- 12lab/serve_user.c
+++ 12lab/serve_user.c
@@ -5,6 +5,7 @@
 #include "serve_user.h"
 #include "validators.h"
 #include "queue_actions.h"
+#include "deque_actions.h"
 
 int handle_user_choice(int user_choice)
 {
@@ -12,5 +13,15 @@ int handle_user_choice(int user_choice)
         case 1:
             printf("\nЗадача №1\n");
             queue_actions_menu();
+            break;
+
+        case 2:
+            printf("\nЗадача №2\n");
+            deque_actions_menu();
+            break;
+
     }
+
+    return 1;
+
 }
