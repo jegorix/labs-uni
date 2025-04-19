7ea331d7a4e3001be7e2a5f9015deafa8d604d73
jegorix <0311egor@gmail.com>
2025-04-19 17:46:25 +0300

    handle 3rd task -> queue identity

--- 12lab/serve_user.c
+++ 12lab/serve_user.c
@@ -2,10 +2,13 @@
 // Created by jegorix on 18.04.25.
 //
 
+
+
 #include "serve_user.h"
 #include "validators.h"
 #include "queue_actions.h"
 #include "deque_actions.h"
+#include "queue_identity.h"
 
 int handle_user_choice(int user_choice)
 {
@@ -20,9 +23,17 @@ int handle_user_choice(int user_choice)
             deque_actions_menu();
             break;
 
+        case 3:
+            printf("\nЗадача №3\n");
+            queue_identity_menu();
+            break;
+
         case 4:
             printf("\nВыход...\n");
             return 4;
+
+        default:
+            return 0;
 
     }
 
