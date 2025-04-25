7eb380f9f764a0463e90a4d5bc0c9456b90c2b53
jegorix <0311egor@gmail.com>
2025-04-26 00:11:28 +0300

    realise find repeats func in tree

---
+++ 13lab/find_repeats.h
@@ -0,0 +1,13 @@
+//
+// Created by jegorix on 25.04.25.
+//
+
+#ifndef INC_13LAB_FIND_REPEATS_H
+#define INC_13LAB_FIND_REPEATS_H
+#include "tree_functions.h"
+
+void collect_values(Node* root, int* array, int* index);
+void find_repeats(Node* root);
+
+
+#endif //INC_13LAB_FIND_REPEATS_H
